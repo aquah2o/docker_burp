7d56eb4edae96c82518283c930b34753d146fc44
Chaos-Monkey-Island <33322194+Chaos-Monkey-Island@users.noreply.github.com>
2020-01-26 19:27:53 -0800

    Update run.sh

--- _data/run.sh
+++ _data/run.sh
@@ -7,7 +7,7 @@ ls -al
 ls -al .BurpSuite
 if [ "$ENV_BURP_PRO" -eq "1" ]; then
     echo "[+] Running Burp Pro"
-    java -jar -Xmx${ENV_MEM_JVM}  /home/tester/sources/burp_pro.jar -Djava.awt.headless=true
+    java -jar -Xmx${ENV_MEM_JVM} -Djava.awt.headless=true /home/tester/sources/burp_pro.jar  --project-file="rabbit.burp"
 else
     echo "[+] Running Burp Free"
     java -jar -Xmx${ENV_MEM_JVM}  /home/tester/sources/burp_free.jar
