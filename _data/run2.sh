84be5e7d5f297c4f063357e99a7a5e33176c8040
Chaos-Monkey-Island <chaos-monkey-island@protonmail.com>
2020-12-29 15:45:55 -0800

    Update run2.sh

--- _data/run2.sh
+++ _data/run2.sh
@@ -11,8 +11,10 @@ echo "[+] JVM Mem: ${ENV_MEM_JVM}"
 
 if [ "$ENV_BURP_PRO" -eq "1" ]; then
     echo "[+] Running Burp Pro"
-    java -jar -Xmx${ENV_MEM_JVM}  /home/tester/sources/burp_pro.jar
+       java -jar -Xmx${ENV_MEM_JVM} /home/tester/sources/burp-rest-api-2.0.1.jar --address=0.0.0.0 --port=8090  --headless.mode=false --unpause-spider-and-scanner --burp.jar=/home/tester/sources/burp_pro.jar 
+#    java -jar -Xmx${ENV_MEM_JVM}  /home/tester/sources/burp_pro.jar
 else
     echo "[+] Running Burp Free"
-    java -jar -Xmx${ENV_MEM_JVM}  /home/tester/sources/burp_free.jar
+    java -jar -Xmx${ENV_MEM_JVM} /home/tester/sources/burp-rest-api-2.0.1.jar --address=0.0.0.0 --port=8090  --headless.mode=false --unpause-spider-and-scanner --burp.jar=/home/tester/sources/burp_pro.jar 
+#   java -jar -Xmx${ENV_MEM_JVM}  /home/tester/sources/burp_free.jar
 fi
