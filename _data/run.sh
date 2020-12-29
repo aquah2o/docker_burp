397640c316806cc51aec9bdc9200192401c50921
Chaos-Monkey-Island <chaos-monkey-island@protonmail.com>
2020-12-29 15:45:09 -0800

    Update run.sh

--- _data/run.sh
+++ _data/run.sh
@@ -2,6 +2,7 @@ mkdir /root/.java
 mkdir /root/.java/.userPrefs
 mkdir /root/.java/.userPrefs/burp
 ls -l
+echo "THIS IS THE STUFF"
 cp ./prefs.xml  /root/.java/.userPrefs/burp/prefs.xml
 ls -la /root/home/tester/.java/.userPrefs/burp/prefs.xml
 if test -z "$ENV_MEM_JVM"; then
@@ -13,9 +14,10 @@ ls -al
 ls -al .BurpSuite
 if [ "$ENV_BURP_PRO" -eq "1" ]; then
     echo "[+] Running Burp Pro"
-    java -jar -Xmx${ENV_MEM_JVM} /home/tester/sources/burp-rest-api-2.0.1.jar --address=0.0.0.0 --port=8090 --server.port=8008 --headless.mode=false --unpause-spider-and-scanner --burp.jar=/home/tester/sources/burp_pro.jar 
+    java -jar -Xmx${ENV_MEM_JVM} /home/tester/sources/burp-rest-api-2.0.1.jar --address=0.0.0.0 --port=8090  --headless.mode=false --unpause-spider-and-scanner --burp.jar=/home/tester/sources/burp_pro.jar 
 #    java -jar -Xmx${ENV_MEM_JVM} -Djava.awt.headless=true /home/tester/sources/burp_pro.jar  --project-file="rabbit.burp"
 else
     echo "[+] Running Burp Free"
-    java -jar -Xmx${ENV_MEM_JVM}  /home/tester/sources/burp_free.jar
+    java -jar -Xmx${ENV_MEM_JVM} /home/tester/sources/burp-rest-api-2.0.1.jar --address=0.0.0.0 --port=8090 --headless.mode=false --unpause-spider-and-scanner --burp.jar=/home/tester/sources/burp_pro.jar 
+#  java -jar -Xmx${ENV_MEM_JVM}  /home/tester/sources/burp_free.jar
 fi
