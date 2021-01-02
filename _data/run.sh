5cbf0650ed66d4cc0ccf39dad14030c8e5dd56a6
Chaos-Monkey-Island <chaos-monkey-island@protonmail.com>
2021-01-02 15:07:07 -0800

    Update run.sh

--- _data/run.sh
+++ _data/run.sh
@@ -12,10 +12,10 @@ fi
 echo "[+] JVM Mem: ${ENV_MEM_JVM}"
 if [ "$ENV_BURP_PRO" -eq "1" ]; then
     echo "[+] Running Burp 1"
-    java -jar -Xmx${ENV_MEM_JVM} /home/tester/sources/burp-rest-api-2.0.1.jar --address=0.0.0.0 --port=8090  --headless.mode=true --unpause-spider-and-scanner --burp.jar=/home/tester/sources/burp_pro.jar 
+    java -jar -Xmx${ENV_MEM_JVM} /home/tester/sources/burp-rest-api-2.0.1.jar --address=0.0.0.0 --port=8090  --headless.mode=false --unpause-spider-and-scanner --burp.jar=/home/tester/sources/burp_pro.jar 
 #    java -jar -Xmx${ENV_MEM_JVM} -Djava.awt.headless=true /home/tester/sources/burp_pro.jar  --project-file="rabbit.burp"
 else
     echo "[+] Running Burp 0"
-    java -jar -Xmx${ENV_MEM_JVM} /home/tester/sources/burp-rest-api-2.0.1.jar --address=0.0.0.0 --port=8090 --headless.mode=true --unpause-spider-and-scanner --burp.jar=/home/tester/sources/burp_pro.jar 
+    java -jar -Xmx${ENV_MEM_JVM} /home/tester/sources/burp-rest-api-2.0.1.jar --address=0.0.0.0 --port=8090 --headless.mode=false --unpause-spider-and-scanner --burp.jar=/home/tester/sources/burp_pro.jar 
 #  java -jar -Xmx${ENV_MEM_JVM}  /home/tester/sources/burp_free.jar
 fi
