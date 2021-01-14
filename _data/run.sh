76c0ee01fb8ada0fc86e971beb84808f0f866f0c
Chaos-Monkey-Island <chaos-monkey-island@protonmail.com>
2021-01-13 23:12:28 -0500

    Update run.sh

--- _data/run.sh
+++ _data/run.sh
@@ -6,16 +6,17 @@ cp /root/prefs.xml  /root/.java/.userPrefs/burp/prefs.xml
 echo "where is ./PREFS"
 sudo find  / -name  prefs.xml -printf "%T+\t%p\n" | sort 
 ls -la /root/.java/.userPrefs/burp/prefs.xml
+cd /home/tester/sources/
 chmod 777 burp-rest-api.sh
-pwd
 if test -z "$ENV_MEM_JVM"; then
     ENV_MEM_JVM=1024m
 fi
 echo "[+] JVM Mem: ${ENV_MEM_JVM}"
+pwd
 if [ "$ENV_BURP_PRO" -eq "1" ]; then
     echo "[+] Running Burp 1"
     ./burp-rest-api.sh
-    java -jar -Xmx${ENV_MEM_JVM} /home/tester/sources/burp-rest-api-2.0.1.jar --address=0.0.0.0 --port=8090  --headless.mode=false --unpause-spider-and-scanner --burp.jar=/home/tester/sources/burp_pro.jar 
+#    java -jar -Xmx${ENV_MEM_JVM} /home/tester/sources/burp-rest-api-2.0.1.jar --address=0.0.0.0 --port=8090  --headless.mode=false --unpause-spider-and-scanner --burp.jar=/home/tester/sources/burp_pro.jar 
 #    java -jar -Xmx${ENV_MEM_JVM} -Djava.awt.headless=true /home/tester/sources/burp_pro.jar  --project-file="rabbit.burp"
 else
     echo "[+] Running Burp 0"
