a4570022b6daf746eeb6cde3eb7368a83cfcc2cb
Chaos-Monkey-Island <chaos-monkey-island@protonmail.com>
2021-01-16 13:32:40 -0500

    Update run.sh

--- _data/run.sh
+++ _data/run.sh
@@ -16,7 +16,7 @@ if test -z "$ENV_MEM_JVM"; then
 fi
 echo "[+] JVM Mem: ${ENV_MEM_JVM}"
 pwd
-tail -F /dev/null
+# uncomment to halt docker steps and leave running -->tail -F /dev/null
 if [ "$ENV_BURP_PRO" -eq "1" ]; then
     echo "[+] Running Burp 1"
 #    ./burp-rest-api.sh --headless.mode=false --address=0.0.0.0 --port=8090  --unpause-spider-and-scanner --config-file="burp_settings.json"
