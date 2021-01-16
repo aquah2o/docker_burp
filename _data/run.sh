ee013006e3fda6bba4155ef9eb303179b317b6fd
Chaos-Monkey-Island <chaos-monkey-island@protonmail.com>
2021-01-16 10:33:54 -0500

    Update run.sh

--- _data/run.sh
+++ _data/run.sh
@@ -1,11 +1,11 @@
 echo "MKDIR .JAVA"
-mkdir /home/tester/.java
-mkdir /home/tester/.java/.userPrefs
-mkdir /home/tester/.java/.userPrefs/burp
+mkdir /root/.java
+mkdir /root/.java/.userPrefs
+mkdir /root/.java/.userPrefs/burp
 echo "COPY FROM /root/ down"
-cp /root/prefs.xml  /home/tester/.java/.userPrefs/burp/prefs.xml
+cp /root/prefs.xml  /root/.java/.userPrefs/burp/prefs.xml
 echo "where is ./PREFS"
-chmod 444 /home/tester/.java/.userPrefs/burp/prefs.xml 
+chmod 444 /root/.java/.userPrefs/burp/prefs.xml 
 sudo find  / -name  prefs.xml -printf "%T+\t%p\n" | sort 
 ls -la /home/tester/.java/.userPrefs/burp/prefs.xml
 cd /home/tester/sources/
