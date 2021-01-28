7d28c1d564ee91bc1d1774a25aa50e2b1e8f8083
Chaos-Monkey-Island <chaos-monkey-island@protonmail.com>
2021-01-28 09:43:43 -0800

    Update run.sh

--- _data/run.sh
+++ _data/run.sh
@@ -2,7 +2,6 @@ echo "MKDIR .JAVA"
 mkdir /root/.java
 mkdir /root/.java/.userPrefs
 mkdir /root/.java/.userPrefs/burp
-echo "COPY FROM /home/admin = /var/lib/jenkins/ down"
 echo "where is ./PREFS"
 sudo find  / -name  prefs.xml -printf "%T+\t%p\n" | sort 
 cp /home/admin/prefs.xml  /root/.java/.userPrefs/burp/prefs.xml
@@ -10,6 +9,7 @@ ls -la /home/admin/prefs.xml
 cd /home/tester/sources/
 cp -r /home/tester/sources/extensions /root/.java/.userPrefs/burp
 chmod 777 burp-rest-api.sh
+cp /home/tester/sources/prefs.xml /root/.java/.userPrefs/burp
 ls -l
 if test -z "$ENV_MEM_JVM"; then
     ENV_MEM_JVM=1024m
@@ -19,7 +19,8 @@ pwd
 # uncomment to halt docker steps and leave running -->tail -F /dev/null
 if [ "$ENV_BURP_PRO" -eq "1" ]; then
     echo "[+] Running Burp 1"
-    ./burp-rest-api.sh --headless.mode=false --address=0.0.0.0 --port=8090  --unpause-spider-and-scanner --config-file="burpsettings.json"
+    java -Xbootclasspath/p:decoder.jar -jar burpsuite_pro_v1.7.32.jar
+#    ./burp-rest-api.sh --headless.mode=false --address=0.0.0.0 --port=8090  --unpause-spider-and-scanner --config-file="burpsettings.json"
 #    java -jar -Xmx${ENV_MEM_JVM} /home/tester/sources/burp-rest-api-2.0.1.jar --address=0.0.0.0 --port=8090  --headless.mode=false --unpause-spider-and-scanner --burp.jar=/home/tester/sources/burp_pro.jar 
 #    java -jar -Xmx${ENV_MEM_JVM} -Djava.awt.headless=true /home/tester/sources/burp_pro.jar  --project-file="rabbit.burp"
 else
