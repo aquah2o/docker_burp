c7ee471091f994554a4e46f85ec28580758f3feb
Chaos-Monkey-Island <chaos-monkey-island@protonmail.com>
2021-02-01 10:58:20 -0500

    Update burp-rest-api.sh

--- _data/sources/burp-rest-api.sh
+++ _data/sources/burp-rest-api.sh
@@ -3,4 +3,5 @@ SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 echo $SCRIPTPATH
 CLASSPATH="$(echo $SCRIPTPATH/build/libs/*.jar | tr ' ' ':'):$(echo $SCRIPTPATH/*.jar | tr ' ' ':')"
 echo $CLASSPATH
-java -cp "$CLASSPATH" org.springframework.boot.loader.JarLauncher $@
+# java -cp "$CLASSPATH" org.springframework.boot.loader.JarLauncher $@
+java -cp burp-rest-api-2.1.0.jar:BurpLoader.jar:burpsuite_pro.jar larry.lau.BurpLoader
